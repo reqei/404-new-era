a48b5badf726b71bd1a555eb7f7a3555f3abe897
최예린 <a01075335013@gmail.com>
2026-06-06 19:53:53 +0900

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
       <body>
        <Header />
 
-        <main className="w-full max-w-[1500px] mx-auto px-6">V
+        <main className="w-full max-w-[1500px] mx-auto px-6">
           {children}
         </main>
       </body>
